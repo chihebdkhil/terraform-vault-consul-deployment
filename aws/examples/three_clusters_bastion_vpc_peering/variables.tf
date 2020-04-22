12a1209ea46636b2d6e7b794e332e94ad3acfe71
chihebdkhil <63354403+chihebdkhil@users.noreply.github.com>
2020-04-22 15:01:36 -0400

    done

--- aws/examples/three_clusters_bastion_vpc_peering/variables.tf
+++ aws/examples/three_clusters_bastion_vpc_peering/variables.tf
@@ -3,10 +3,10 @@ variable "tags" {
 }
 variable "consul_ent_license" {}
 variable "region1" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 variable "region2" {
-  default = "us-east-2"
+  default = "us-west-2"
 }
 variable "region3" {
   default = "eu-central-1"
